e967b4ca822ddc64f5f6b3f0bbf67389116e382a
Avasam <samuel.06@hotmail.com>
2023-03-22 01:28:59 -0400

    CLI that reuses other tools

---
+++ Texture packs/Dolphin PC texture pack generator/Console Generator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using PCTexturePackGeneratorConsole;
+
+(var pcArcFolder, var outputLocation) = Utils.ParseArguments(args);
+var workingPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+var tempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+var extractedTexturesFolder = $"{tempDirectory}/textures";
+var convertedTexturesFolder = $"{tempDirectory}/textures_converted";
+Directory.CreateDirectory(tempDirectory);
+
+// PitfallARCTool.Program.Main(new string[]{ "-x", $"{pcArcFolder}/textures.arc", "-o", extractedTexturesFolder });
+var exitCode = Utils.Execute($"{workingPath}\\PitfallARCTool.exe", $"-x \"{pcArcFolder}/textures.arc\" -o \"{extractedTexturesFolder}\" -e LE");
+if (exitCode != 0) return exitCode;
+
+// TexConvert.TexConvert.Main(new string[] { extractedTexturesFolder, convertedTexturesFolder });
+exitCode = Utils.Execute($"{workingPath}\\TexConvert.exe", $"{extractedTexturesFolder} {convertedTexturesFolder}");
+if (exitCode != 0) return exitCode;
+
+foreach (var textureFileInfo in new DirectoryInfo(convertedTexturesFolder).GetFiles())
+{
+  // var newName = TextureNameMapping.Dict.GetValueOrDefault(textureFileInfo.Name);
+  var newName = textureFileInfo.Name;
+  if (string.IsNullOrEmpty(newName))
+  {
+    File.Delete(textureFileInfo.FullName);
+  }
+  else
+  {
+    var destinationFolder = $"{outputLocation}/GPHE/";
+    if (!File.Exists(destinationFolder))
+    {
+      Directory.CreateDirectory(destinationFolder);
+    }
+    File.Move(textureFileInfo.FullName, destinationFolder + newName);
+  }
+}
+
+return 0;
