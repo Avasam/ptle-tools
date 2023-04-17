2872019341a5db5a1ae62a976208016fcaddf5ac
Avasam <samuel.06@hotmail.com>
2023-04-17 10:30:16 -0400

    Generated DTE texture map

---
+++ Texture packs/Dolphin PC texture pack generator/map_textures_from_DTE_log.ps1
@@ -0,0 +1,25 @@
+# Reads a DolphinTextureExtraction.log file to map the textures' original names to their Dolphin equivalent hashed-names.
+# Used to generate DTE_texture_map.json/xml.
+
+param ([string]$dteLogFile)
+if (-not $args) {
+  # $dteLogFile = Read-Host 'Enter the path to DolphinTextureExtraction.log'
+  $dteLogFile = 'E:\Users\Avasam\Documents\ROMs & Mods\PC\Pitfall-TLE\ARCs\Gamecube_US\files\~textures_2\DolphinTextureExtraction.log'
+}
+else {
+  $dteLogFile = $args[0]
+}
+
+$map = @{}
+Get-Content $dteLogFile
+| Where-Object { $_.StartsWith('[01] Extract: ') }
+| ForEach-Object { $_.Substring(14) }
+| ForEach-Object {
+  $key, $value = $_.Split('\')
+  if (!$map.ContainsKey($key)) {
+    $map[$key] = @()
+  }
+  $map[$key] += $value
+}
+
+$map | ConvertTo-Json | Out-File "$PSScriptRoot/Console Generator/DTETextureMap.json"
