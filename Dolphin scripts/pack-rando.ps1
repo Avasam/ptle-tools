0cc0a0f28840f43d97323109296ce4eea32acbd5
Avasam <samuel.06@hotmail.com>
2024-06-14 15:22:17 -0400

    Add version number to created rando pack

--- Dolphin scripts/pack-rando.ps1
+++ Dolphin scripts/pack-rando.ps1
@@ -1,7 +1,9 @@
 $ScriptsFolder = "$PSScriptRoot\Scripts"
 $RandoFolderName = 'Entrance Randomizer'
+$VersionLine = Get-Content "$PSScriptRoot\$RandoFolderName/lib/constants.py" | Select-String -Pattern '^__version__\s*=\s*".*"'
+$RandoVersion = $VersionLine -replace '^\s*__version__\s*=\s*"', '' -replace '".*$', ''
 Remove-Item -Path $ScriptsFolder -Recurse -ErrorAction SilentlyContinue
 Copy-Item -Path "$PSScriptRoot\$RandoFolderName" -Destination "$ScriptsFolder\$RandoFolderName" -Recurse
 Copy-Item -Path "$PSScriptRoot\..\Various technical notes\transition_infos.json" -Destination "$ScriptsFolder\$RandoFolderName\lib"
-Compress-Archive -Path $ScriptsFolder -DestinationPath $RandoFolderName -Force
+Compress-Archive -Path $ScriptsFolder -DestinationPath "$RandoFolderName v$RandoVersion.zip" -Force
 Remove-Item -Path $ScriptsFolder -Recurse
