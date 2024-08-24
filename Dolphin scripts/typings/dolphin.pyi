c80f9b0dc235d41568852a2b11aa7ec49bfaa1aa
Avasam <samuel.06@hotmail.com>
2024-08-24 18:11:15 -0400

    Extract Ruff config to won file and sync with skeleton (#82)

--- Dolphin scripts/typings/dolphin.pyi
+++ Dolphin scripts/typings/dolphin.pyi
@@ -1,21 +1,31 @@
 """
 Aggregator module of all dolphin-provided modules.
+
 It lets people import the dolphin-provided modules in a more
 intuitive way. For example, people can then do this:
-    `from dolphin import event, memory`
+```
+from dolphin import event, memory
+```
 instead of:
-    `import dolphin_event as event`
-    `import dolphin_memory as memory`
+```
+import dolphin_event as event
+import dolphin_memory as memory
+```
 
 Valid:
-  `import dolphin`
-  `from dolphin import *`
-  `from dolphin import event`
-  `import dolphin_event as event`
+```
+import dolphin
+from dolphin import *
+from dolphin import event
+import dolphin_event as event
+```
 Invalid:
-  `import dolphin.event`
-  `from dolphin.event import ...`
-"""  # noqa: D400,D415 # Tries to add a . at the end of an import line
+```
+import dolphin.event
+from dolphin.event import ...
+```
+"""
+
 import dolphin_controller as controller
 import dolphin_event as event
 import dolphin_gui as gui
